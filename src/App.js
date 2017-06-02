534c3629bf51d3313cb9080d977748e4240258de
Tobi <tobiadeyemi91@gmail.com>
2017-06-02 23:44:45 +0100

    split app component created Search and Table component from split

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
   constructor (props) {
     super(props);
 
+    //app component's state
     this.state = {
       list,
       searchTerm: '',
@@ -62,52 +63,70 @@ class App extends Component {
       firstname: 'Olutobi Adeyemi',
       age: 26
     };
+    //using destructuring to access the state
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
           <h2>{helloWorld}</h2>
-          <form>
-            <input type="text" onChange={this.onSearchChange} />
-          </form>
           <p> {`Hey ${user.firstname}, I hear you are ${user.age} years old, right?`} </p>
-          { this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span> 
-                  <button 
-                    onClick={() => this.onDismiss(item.objectID)}
-                    type="button"
-                  >
-                    Dimiss
-                  </button>
-                </span>
-              </div>
-          )}
-          {/*{ this.state.list.map( item => 
-              <div key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span> 
-                  <button 
-                    onClick={() => this.onDismiss(item.objectID)}
-                    type="button"
-                  >
-                    Dimiss
-                  </button>
-                </span>
-              </div>
-          )}*/}
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+          <Table 
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+          
       </div>
     );
   }
 }
 
+//search component
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input 
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    )
+  }
+}
+
+
+//table component
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        { list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
 export default App;
